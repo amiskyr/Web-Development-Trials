8b297c3b1359188e23a2c279525a543975ab917f
SKY <akash.rm.roy@gmail.com>
2021-01-15 17:15:53 +0530

    Async Javascript Promises demo

---
+++ Async Javascript/Promises/Creating Promises/app.js
@@ -0,0 +1,29 @@
+const delayedColorChange = (newColor, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = newColor;
+            resolve();
+        }, delay)
+    })
+}
+
+// delayedColorChange("red", 1000, () => {
+//     delayedColorChange("orange", 1000, () => {
+//         delayedColorChange("yellow", 1000, () => {
+//             delayedColorChange("green", 1000, () => {
+//                 delayedColorChange("blue", 1000, () => {
+
+//                 })
+//             })
+//         })
+//     })
+// });
+
+// chaining the promise object methods onto the returned promis objects
+delayedColorChange("red", 1000)
+    .then(() => delayedColorChange("orange", 1000))
+    .then(() => delayedColorChange("yellow", 1000))
+    .then(() => delayedColorChange("green", 1000))
+    .then(() => delayedColorChange("blue", 1000))
+    .then(() => delayedColorChange("indigo", 1000))
+    .then(() => delayedColorChange("violet", 1000))
