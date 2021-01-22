54a37bc4918f9f2c87f782e641ab9ead3b072333
SKY <akash.rm.roy@gmail.com>
2021-01-22 23:00:05 +0530

    Update app.js

--- AJAX and APIs/Axios/app.js
+++ AJAX and APIs/Axios/app.js
@@ -1,35 +1,44 @@
 let url = "https://api.cryptonator.com/api/ticker/btc-usd";
 
+// basic axiom function 'get()' to access the data through get-request
 // axios.get(url)
 //     .then((res) => {
 //         console.log(res.data.ticker.price);
 //     })
 //     .catch((err) => {
-//         console.log("ERROE!", err);
+//         console.log("ERROR!", err);
 //     })
 
-const fetchBitcoinPrice = async () => {
-    try {
-        const res = await axios.get(url);
-        console.log(res.data.ticker.price);
-    }
-    catch (e) {
-        console.log("ERROR!", e)
-    }
-}
+// Async function based implementation for fetching json data from "cryptonator.com/api/"
+// const fetchBitcoinPrice = async () => {
+//     try {
+//         const res = await axios.get(url);
+//         console.log(res.data.ticker.price);
+//     }
+//     catch (e) {
+//         console.log("ERROR!", e)
+//     }
+// }
 
+// referencing the <ul> element with identifier="jokes"
 const jokes = document.querySelector("#jokes");
+
+// referencing the only button object in document
 const button = document.querySelector("button");
 
+// Addition of new joke by appending the joke text to the new <LI> element
 const addNewJoke = async () => {
     const jokeText = await getDadJoke();
 
+    //new <LI> element created
     const newLI = document.createElement("LI");
+
     console.log(jokeText);
     newLI.append(jokeText);
     jokes.append(newLI);
 }
 
+// async function to retrieve the json data from the request to api
 const getDadJoke = async () => {
     try {
         // headers are used to change the request specifications
@@ -48,4 +57,5 @@ const getDadJoke = async () => {
     }
 }
 
+// Function assigned to onClick event of the button
 button.addEventListener("click", addNewJoke);
