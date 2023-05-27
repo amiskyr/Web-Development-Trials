697cffc380ccc6091decaf02cd53438a08582c24
SKY-ROY <akash.rm.roy@gmail.com>
2023-05-27 16:32:09 +0530

    POST request handling Trial done

---
+++ RESTful Routes/REST Trial/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+// this middleware will parse the request body with urlencoded payload data
+app.use(express.urlencoded({ extended: true }))
+// this middleware will parse the request body with json payload data
+app.use(express.json())
+
+app.get('/tacos', (req, res) => {
+    res.send("GET /tacos response")
+})
+
+app.post('/tacos', (req, res) => {
+    // console.log(req.body)
+    const { meat, qty } = req.body
+    res.send(`OK, here are your ${qty} ${meat} tacos. `)
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
