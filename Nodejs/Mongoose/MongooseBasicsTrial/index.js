a7a7d4b24c9c81e8f8df28d9cf0faa6d5afe9fae
SKY-ROY <akash.rm.roy@gmail.com>
2023-06-09 00:30:43 +0530

    CRUD operations trial completed

--- Nodejs/Mongoose/MongooseBasicsTrial/index.js
+++ Nodejs/Mongoose/MongooseBasicsTrial/index.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
+const macURI = 'mongodb://root:example@127.0.0.1:27017'
+const winURI = 'mongodb://127.0.0.1:27017/movieApp'
+
 // setup mongoose connection
-mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
-    .then(() => {
-        console.log('connection open')
+mongoose.connect(winURI)
+    .then((data) => {
+        console.log('---connection open---\n')
+        // console.log(data)
     })
     .catch(err => {
-        console.log('connection failed')
+        console.log('---connection failed---\n')
+        console.log(err)
     });
 
 // defining a mongoose schema
@@ -21,17 +26,18 @@ const movieSchema = new mongoose.Schema({
 const Movie = mongoose.model('Movie', movieSchema)
 
 // declaring an instance of Movie class
-// const interstellar = new Movie({ title: "Interstellar", year: 2014, score: 9, rating: "U" })
+// const amadeus = new Movie({ title: "Amadeus", year: 1986, score: 9, rating: "U" })
 
-Movie.insertMany([
-    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
-    { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
-    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
-    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
-    { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
-])
-    .then(data => {
-        console.log("Insterted multiple movies")
-        console.log(data)
-    });
+// inserting multiple objects at once
+// Movie.insertMany([
+//     { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+//     { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+//     { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+//     { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+//     { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+// ])
+//     .then(data => {
+//         console.log("Insterted multiple movies")
+//         console.log(data)
+//     });
 
