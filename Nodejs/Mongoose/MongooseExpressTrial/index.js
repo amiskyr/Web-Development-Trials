4c65532b005b62bf6eef203fcb5dd9c76b4b11b5
akash_roy <akash.rm.roy@gmail.com>
2023-06-11 02:37:14 +0530

    Mongoose + Express trial

---
+++ Nodejs/Mongoose/MongooseExpressTrial/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+// const mongoose = require('mongoose');
+// const Product = require('./models/product');
+
+// const macURI = 'mongodb://root:example@127.0.0.1:27017'
+// const winURI = 'mongodb://127.0.0.1:27017/farmStand'
+
+// // setup mongoose connection
+// mongoose.connect(winURI)
+//     .then((data) => {
+//         console.log('---mongo connection open---\n')
+//         // console.log(data)
+//     })
+//     .catch(err => {
+//         console.log('---mongo connection failed---\n')
+//         console.log(err)
+//     });
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/dog', (req, res) => {
+    res.send('Woof')
+})
+
+app.listen(3000, () => {
+    console.log('App is listening on port 3000')
+})
