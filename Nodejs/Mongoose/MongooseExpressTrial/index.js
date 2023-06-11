35edeb6b59d3183e9786a86461eee90a9b8be051
akash_roy <akash.rm.roy@gmail.com>
2023-06-11 18:56:12 +0530

    Mongoose+ Express Trial >pre selected category highlight in edit page implemented

--- Nodejs/Mongoose/MongooseExpressTrial/index.js
+++ Nodejs/Mongoose/MongooseExpressTrial/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const methodOverride = require('method-override')
 const Product = require('./models/product')
 const mongoose = require('mongoose');
 
 const macURI = 'mongodb://root:example@127.0.0.1:27017'
 const winURI = 'mongodb://127.0.0.1:27017/farmStand'
+const categories = ['fruit', 'vegetable', 'dairy']
 
 // setup mongoose connection
 mongoose.connect(winURI)
@@ -20,7 +22,9 @@ mongoose.connect(winURI)
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
@@ -28,7 +32,7 @@ app.get('/products', async (req, res) => {
 })
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new')
+    res.render('products/new', { categories })
 })
 
 app.post('/products', async (req, res) => {
@@ -41,8 +45,20 @@ app.post('/products', async (req, res) => {
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params
     const productFound = await Product.findById(id)
-    console.log(productFound)
     res.render('products/show', { productFound })
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const foundProduct = await Product.findById(id)
+    res.render('products/edit', { foundProduct, categories })
+})
+
+app.put('/products/:id', async (req, res) => {
+    console.log(req.body)
+    const { id } = req.params
+    const updatedProduct = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+    res.redirect(`/products/${updatedProduct._id}`)
 })
 
 app.listen(3000, () => {
