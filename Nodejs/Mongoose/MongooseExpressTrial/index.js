9c3fb3c5e3697e1bc18a5e8663e6b559c31a8140
akash_roy <akash.rm.roy@gmail.com>
2023-06-11 19:22:36 +0530

    Mongoose+ Express Trial >Implemented category wise display filter

--- Nodejs/Mongoose/MongooseExpressTrial/index.js
+++ Nodejs/Mongoose/MongooseExpressTrial/index.js
@@ -27,8 +27,16 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index', { products })
+    const { category } = req.query
+
+    if (category) {
+        const products = await Product.find({ category })
+        res.render('products/index', { products, category })
+    }
+    else {
+        const products = await Product.find({})
+        res.render('products/index', { products, category: 'All' })
+    }
 })
 
 app.get('/products/new', (req, res) => {
