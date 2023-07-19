8758d9120436471df13e7230725528ff7f7bd300
SKY-ROY <akash.rm.roy@gmail.com>
2023-07-20 01:06:31 +0530

    Express Error Trial > Implemented an Async Error Handling utility to  avoid redundant try/catch blocks

--- Nodejs/Expressjs/AsyncErrorHandlingDemo/index.js
+++ Nodejs/Expressjs/AsyncErrorHandlingDemo/index.js
@@ -27,10 +27,16 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-// Index listing all products
-app.get('/products', async (req, res) => {
-    const { category } = req.query
+// Async utility
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch(e => next(e))
+    }
+}
 
+// Index listing all products
+app.get('/products', wrapAsync(async (req, res, next) => {
+    const { category } = req.query
     if (category) {
         const products = await Product.find({ category })
         res.render('products/index', { products, category })
@@ -39,60 +45,57 @@ app.get('/products', async (req, res) => {
         const products = await Product.find({})
         res.render('products/index', { products, category: 'All' })
     }
-})
+}))
 
 // Add a new product use GET request
 app.get('/products/new', (req, res) => {
-    throw new AppError('Not Allowed', 401)
     res.render('products/new', { categories })
 })
 
 // Submit the form of new product details to be added using POST request
-app.post('/products', async (req, res) => {
+app.post('/products', wrapAsync(async (req, res, next) => {
     const newProduct = new Product(req.body)
     await newProduct.save()
-    console.log(newProduct)
+    // console.log(newProduct)
     res.redirect(`/products/${newProduct._id}`)
-})
+}))
 
 // Product details using _id in DB
-app.get('/products/:id', async (req, res, next) => {
+app.get('/products/:id', wrapAsync(async (req, res, next) => {
     const { id } = req.params
     const productFound = await Product.findById(id)
 
     if (!productFound) {
         // throw new AppError('Product not found', 404)    // This will not work in case of sync functions and their errors
-        return next(new AppError('Product not found', 404))    // This will trigger the error handling middleware and work accordingly
+        throw new AppError('Product not found', 404)    // This will trigger the error handling middleware and work accordingly
     }
     res.render('products/show', { productFound })
-
-})
+}))
 
 // Edit product details by and opening the edit page on GET request
-app.get('/products/:id/edit', async (req, res, next) => {
+app.get('/products/:id/edit', wrapAsync(async (req, res, next) => {
     const { id } = req.params
     const foundProduct = await Product.findById(id)
 
     if (!foundProduct) {
-        return next(new AppError('Product not found', 404))    // This will trigger the error handling middleware and work accordingly
+        throw new AppError('Product not found', 404)    // This will trigger the error handling middleware and work accordingly
     }
     res.render('products/edit', { foundProduct, categories })
-})
+}))
 
 // Submission of the updated product details form using PUT request (method-override) to update in DB
-app.put('/products/:id', async (req, res) => {
-    console.log(req.body)
+app.put('/products/:id', wrapAsync(async (req, res, next) => {
     const { id } = req.params
     const updatedProduct = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
     res.redirect(`/products/${updatedProduct._id}`)
-})
+}))
 
 // Deletion of the product using DELETE request (method-override) from DB
-app.delete('/products/:id', async (req, res) => {
+app.delete('/products/:id', wrapAsync(async (req, res) => {
     const { id } = req.params
     const deletedProduct = await Product.findByIdAndDelete(id)
     res.redirect('/products')
-})
+}))
 
 app.use((err, req, res, next) => {
     const { status = 500, message = 'Something went wrong!' } = err    // setting the default value so in case of undefined status we will respond with this
@@ -101,4 +104,4 @@ app.use((err, req, res, next) => {
 
 app.listen(3000, () => {
     console.log('App is listening on port 3000')
-})
+}) 
