75e3ede1c140193b450090d91360796f0b016b23
amiskyr <akash.rm.roy@gmail.com>
2024-01-23 10:16:22 +0530

    Cookies with Express > Implemented signing of cookies and their verification

--- Nodejs/Expressjs/CookiesDemo/index.js
+++ Nodejs/Expressjs/CookiesDemo/index.js
@@ -3,7 +3,8 @@ const app = express();
 
 const cookieParser = require('cookie-parser')
 
-app.use(cookieParser());
+// passing a secret key which will be used to sign the cookies and ensure that the data integrity(unchanged cookie content) is maintained by verifying the signed cookie received from client.
+app.use(cookieParser('thisismysecret'));
 
 app.get('/greet', (req, res) => {
     // parsing a cookie
@@ -15,6 +16,22 @@ app.get('/setname', (req, res) => {
     // setting and sending a cookie to browser
     res.cookie('name', 'askyr')
     res.send('Okay, sent you a cookie!')
+})
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', { signed: true })
+    res.send('Ok, signed your fruit cookie!')   // cookie received on client end : "s%3Agrape.LMNZojp%2FiR9Tsj50P0ysA22deJjrP0awUK0S8R3lTUk"
+})
+
+app.get('/verifyfruit', (req, res) => {
+    // req.cookies will only show unsigned cookies 
+    console.log(req.cookies)
+
+    // req.signedCookies will rather show signed cookies only
+    console.log(req.signedCookies)
+
+    // if we try to change the cookie content manually from browser this will send an empty object or {"fruit":false} on omitting the content from signed cookie string
+    res.send(req.signedCookies)
 })
 
 app.listen(3000, () => {
