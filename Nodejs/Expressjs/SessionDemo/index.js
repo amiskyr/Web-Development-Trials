9b9969cff1b4508f691c0ace15b5e0585cee224f
SKY <akash.rm.roy@gmail.com>
2024-02-26 02:04:22 +0530

    Cookies with Express > Implemented Session handling

---
+++ Nodejs/Expressjs/SessionDemo/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+// initializeing express-session 
+const session = require('express-session');
+
+const sessionOptions = {secret: 'thisisnotagoodsecret', resave: false, saveUninitialized: false};
+
+// express session will sign the cookies that it sends back
+app.use(session(sessionOptions));
+
+app.get('/viewcount', (req, res) => {
+    if(req.session.count) {
+        req.session.count += 1;
+    }
+    else {
+        req.session.count = 1;
+    }
+    res.send(`You have viewed this page ${req.session.count} times`);    
+})
+
+app.get('/register', (req, res) => {
+    const {username = 'Anonymous'} = req.query;
+    req.session.username = username;
+    res.redirect('/greet');
+})
+
+app.get('/greet', (req, res) => {
+    const {username} = req.session;
+    res.send(`Welcome back, ${username}`)
+})
+
+app.listen(3000, ()=> {
+    console.log('listening on port 3000');
+})
+
