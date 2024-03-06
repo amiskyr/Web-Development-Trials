cfd4db00f29f23ece8f51ab4fd7d554f310e9682
SKY <akash.rm.roy@gmail.com>
2024-03-07 01:18:23 +0530

    Authentication > Authentication Demo > registeration implemented

--- Nodejs/Expressjs/Authentication/AuthenticationDemo/index.js
+++ Nodejs/Expressjs/Authentication/AuthenticationDemo/index.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const app = express();
-const User = require('./models/user')
+const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const macURI = 'mongodb://root:example@127.0.0.1:27017'
+const winURI = 'mongodb://127.0.0.1:27017/authenticationDemo'
+
+// setup mongoose connection
+mongoose.connect(winURI)
+    .then((data) => {
+        console.log('---mongo connection open---\n')
+        // console.log(data)
+    })
+    .catch(err => {
+        console.log('---mongo connection failed---\n')
+        console.log(err)
+    });
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('This is the homepage')
+})
+
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/');
 })
 
 app.get('/secret', (req, res) => {
