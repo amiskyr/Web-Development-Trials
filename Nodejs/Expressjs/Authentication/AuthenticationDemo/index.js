2f4d2bc00de47f265e2c56d509151a0e36fce5db
SKY <akash.rm.roy@gmail.com>
2024-03-08 01:02:55 +0530

    Authentication > Authentication Demo > Login implemented

--- Nodejs/Expressjs/Authentication/AuthenticationDemo/index.js
+++ Nodejs/Expressjs/Authentication/AuthenticationDemo/index.js
@@ -42,6 +42,22 @@ app.post('/register', async (req, res) => {
     res.redirect('/');
 })
 
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const validPassword = await bcrypt.compare(password, user.password)
+    if (validPassword) {
+        res.send("Yay Welcome!")
+    }
+    else {
+        res.send("Try again!")
+    }
+})
+
 app.get('/secret', (req, res) => {
     res.send(`This is secret, you can't see me unless you're logged in!`)
 })
