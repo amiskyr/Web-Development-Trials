9616787f8bc1652c2176458c399fe2dff21296af
SKY <akash.rm.roy@gmail.com>
2024-03-08 01:17:55 +0530

    Authentication > Authentication Demo > Implemented logged-in state using session

--- Nodejs/Expressjs/Authentication/AuthenticationDemo/index.js
+++ Nodejs/Expressjs/Authentication/AuthenticationDemo/index.js
@@ -3,6 +3,7 @@ const app = express();
 const User = require('./models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 const macURI = 'mongodb://root:example@127.0.0.1:27017'
 const winURI = 'mongodb://127.0.0.1:27017/authenticationDemo'
@@ -22,6 +23,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'notagoodsecret', resave: false, saveUninitialized: false }))
 
 app.get('/', (req, res) => {
     res.send('This is the homepage')
@@ -39,6 +41,7 @@ app.post('/register', async (req, res) => {
         password: hash
     })
     await user.save();
+    req.session.user_id = user._id;
     res.redirect('/');
 })
 
@@ -51,14 +54,21 @@ app.post('/login', async (req, res) => {
     const user = await User.findOne({ username });
     const validPassword = await bcrypt.compare(password, user.password)
     if (validPassword) {
-        res.send("Yay Welcome!")
+        req.session.user_id = user._id;
+        // res.send("Yay Welcome!")
+        res.redirect('/secret');
     }
     else {
-        res.send("Try again!")
+        // res.send("Try again!")
+        res.redirect('/login')
     }
 })
 
+// specific route to test logged in state using session
 app.get('/secret', (req, res) => {
+    if (!req.session.user_id) {
+        res.redirect('/login')
+    }
     res.send(`This is secret, you can't see me unless you're logged in!`)
 })
 
