9b998233e4d8929cd1893a2a94544e2c3b9871aa
SKY <akash.rm.roy@gmail.com>
2024-03-09 02:34:11 +0530

    Authentication > Authentication Demo > Moved User specific methods to user model

--- Nodejs/Expressjs/Authentication/AuthenticationDemo/index.js
+++ Nodejs/Expressjs/Authentication/AuthenticationDemo/index.js
@@ -43,11 +43,7 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
     const { password, username } = req.body;
-    const hash = await bcrypt.hash(password, 12);
-    const user = new User({
-        username,
-        password: hash
-    })
+    const user = new User({ username, password })
     await user.save();
     req.session.user_id = user._id;
     res.redirect('/');
@@ -59,15 +55,12 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    const validPassword = await bcrypt.compare(password, user.password)
-    if (validPassword) {
-        req.session.user_id = user._id;
-        // res.send("Yay Welcome!")
+    const foundUser = await User.findAndValidate(username, password)
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
         res.redirect('/secret');
     }
     else {
-        // res.send("Try again!")
         res.redirect('/login')
     }
 })
--- Nodejs/Expressjs/Authentication/AuthenticationDemo/models/user.js
+++ Nodejs/Expressjs/Authentication/AuthenticationDemo/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -9,6 +10,21 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Password cannot be blank']
     }
+})
+
+// method to find and verify the user credentials
+userSchema.statics.findAndValidate = async function (username, password) {
+    const foundUser = await this.findOne({ username });
+    const isValid = await bcrypt.compare(password, foundUser.password);
+    return isValid ? foundUser : false;
+}
+
+// middleware to hash pw thats runs before saving the user credentials in DB
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password'))
+        return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
 })
 
 module.exports = mongoose.model('User', userSchema);
